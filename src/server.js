d3b486ab38d835f9e531e61bbd335aec216c8e90
unknown <theocombymg@gmail.com>
2018-12-22 23:00:36 +0100

    Commit final pour mise en ligne du site

--- src/server.js
+++ src/server.js
@@ -144,7 +144,7 @@ models.sequelize.sync().then(function() {
    * 
    * Listen only when database connection is sucessfull
    */
-  app.listen(3000, function() {
+  app.listen(process.env.PORT, function() {
     console.log('Express server listening on port 3000');
   });
 });
